fd1959eea7a3afff5aa9d4343e8631815bc5cbab
Jeisiel10 <jeisiel10@hotmail.com>
2016-01-22 09:24:29 -0200

    Organização

---
+++ Organizacao/Sistema/Contas/Conta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organizacao.Sistema.Contas
+{
+	class Conta
+	{
+		public double Saldo { get; set; }
+
+		public void Deposita(double valor)
+		{
+			this.Saldo += valor;
+		}
+
+	}
+}
---
+++ Organizacao/Sistema/Testes/Teste.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Organizacao.Sistema.Contas;
+
+namespace Organizacao.Sistema.Testes
+{
+	class Teste
+	{
+		static void Main(string[] args)
+		{
+			Conta c = new Conta();
+			c.Deposita(100);
+			Console.WriteLine(c.Saldo);
+		}
+	}
+}
